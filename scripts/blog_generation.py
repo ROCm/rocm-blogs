2bb23e723cfe408c87d75c2097e28d4e02af7eaa
Danny Guan <Danny.Guan@amd.com>
2025-02-07 13:59:54 -0500

    Codebase Refactoring (#518)
    Code Base Refactor
    - Giscus
    - Images on blogs
    
    ---------
    
    Co-authored-by: Saad Rahim (AMD) <44449863+saadrahim@users.noreply.github.com>

---
+++ scripts/blog_generation.py
@@ -0,0 +1,132 @@
+from blog import grab_authors, grab_image
+from datetime import datetime
+from giscus import giscus
+from quick_share import quickshare
+
+IMAGE_CSS_PATH = "./scripts/css/image_blog.css"
+IMAGE_HTML_PATH = "./scripts/html/image_blog.html"
+BLOG_CSS_PATH = "./scripts/css/blog.css"
+
+
+def author_attribution(blogs, minimum_date="September 1, 2024"):
+
+    blogs_to_process = []
+
+    for blog in blogs:
+
+        if blog.date < datetime.strptime(minimum_date, "%B %d, %Y"):
+
+            print(f"Skipping {blog.file_path}: Date is before {minimum_date}.")
+
+            continue
+        else:
+
+            blogs_to_process.append(blog)
+    for blog in blogs_to_process:
+
+        authors_list = getattr(blog, "author", "").split(",")
+        date = blog.date.strftime("%B %d, %Y") if blog.date else "No Date"
+
+        if authors_list:
+
+            authors_html = grab_authors(authors_list)
+
+            if authors_html:
+
+                authors_html = authors_html.replace("././", "../../").replace(
+                    ".md", ".html"
+                )
+
+                authors_string = f"{date}, by {authors_html}"
+
+                authors_html = f'<div class="author_string">{authors_string}</div>'
+            # grab blog link
+
+            readme_file = blog.file_path
+
+            with open(readme_file, "r", encoding="utf-8") as file:
+
+                lines = file.readlines()
+            title, line_number = None, None
+
+            for i, line in enumerate(lines):
+
+                # only check for # , do not check if there are more than one # in the line
+
+                if line.startswith("#") and line.count("#") == 1:
+
+                    title = line
+
+                    line_number = i
+
+                    break
+            if title:
+
+                # insert the author attribution after the title
+                # title
+                #
+                # author attribution
+                #
+                # content
+
+                blog_image = None
+
+                # quickshare
+
+                quickshare_button = quickshare(blog)
+
+                with open(IMAGE_CSS_PATH, "r") as f:
+
+                    image_css = f.read()
+                with open(IMAGE_HTML_PATH, "r") as f:
+
+                    image_html = f.read()
+                with open(BLOG_CSS_PATH, "r") as f:
+
+                    blog_css = f.read()
+                blog_template = f"""
+<style>
+{blog_css}
+</style>
+"""
+                image_template = f"""
+<style>
+{image_css}
+</style>
+{image_html}
+"""
+                print(blog.image_paths)
+
+                if blog.image_paths:
+
+                    blog_image = "../../_" + blog.image_paths[0][2:]
+
+                image_template = image_template.replace("{IMAGE}", blog_image).replace(
+                    "{TITLE}", blog.blog_title
+                )
+
+                lines.insert(line_number + 1, f"\n{blog_template}\n")
+
+                lines.insert(line_number + 2, f"\n{image_template}\n")
+
+                lines.insert(line_number + 3, f"\n{authors_html}\n")
+
+                # add the image to html
+
+                lines.insert(line_number + 4, f"\n{quickshare_button}\n")
+
+                giscus_comment = giscus()
+
+                # insert at the end of the file
+
+                lines.append(giscus_comment)
+
+                with open(readme_file, "w", encoding="utf-8") as file:
+
+                    # add date class style
+
+                    file.writelines(lines)
+                print(f"Author attribution inserted in '{readme_file}'.")
+        else:
+
+            print("No authors found in metadata.")
---
+++ scripts/core.py
@@ -0,0 +1,67 @@
+import os
+import time
+
+from blog import *
+from blog_generation import author_attribution
+from grid_generation import generate_blog_grid
+from quick_share import quickshare
+
+
+def main():
+    root_directory = "blogs"  # Specify the root directory
+
+    start_time = time.time()
+
+    # do ls
+
+    print("Current working directory: ", os.getcwd())
+
+    os.chdir("..")
+
+    if not os.path.exists(root_directory):
+
+        print(f"The directory '{root_directory}' does not exist.")
+
+        return
+    print(
+        f"Searching for 'readme.md' files in '{root_directory}' and subdirectories..."
+    )
+
+    readme_files = find_readme_files(root_directory)
+
+    if not readme_files:
+
+        print("No 'readme.md' files found.")
+
+        return
+    print(f"Found {len(readme_files)} 'readme.md' file(s).")
+
+    blogs = create_blog_objects(readme_files)
+
+    # Sort blogs by date
+
+    sorted_blogs = sort_blogs_by_date(blogs)
+
+    for blog in sorted_blogs:
+
+        if hasattr(blog, "author"):
+
+            print(blog.author)
+    # Generate the grid for the top 15 latest blogs
+
+    generate_blog_grid(sorted_blogs)
+
+    author_attribution(sorted_blogs)
+
+    with open("time.txt", "w") as f:
+
+        f.write(f"Time taken: {time.time() - start_time} seconds")
+    print(f"Time taken: {time.time() - start_time} seconds")
+
+    # change back working directory
+
+    os.chdir("blogs")
+
+
+if __name__ == "__main__":
+    main()
